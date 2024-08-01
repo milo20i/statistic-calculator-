49222825dd2c2530f34ddf203964f9842a3b3b29
Danilo De Matteis <62311664+milo20i@users.noreply.github.com>
2024-08-01 18:50:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length
 
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
+}
+
+const getMode = (array) => {
+  const counts = {};
+  array.forEach(el => {
+
+  })
+  return array;
 }
 
 const calculate = () => {
@@ -10,6 +23,9 @@ const calculate = () => {
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
 
   const mean = getMean(numbers);
+  const median = getMedian(numbers);
+  console.log(getMode(numbers));
 
   document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
 }
